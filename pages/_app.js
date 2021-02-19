597d2fcedbbe9ecc4d200b7a63061540d7b9e889
imad <imadatyatalah@gmail.com>
2021-02-19 15:54:14 +0100

    small fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default MyApp;
