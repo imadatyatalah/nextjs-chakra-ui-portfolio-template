0a116d0c13aaa8a55905584540633996550f12a2
imad <imadatyatalah@gmail.com>
2021-02-19 21:31:22 +0100

    config chakra ui

---
+++ src/components/theme/colorMode.jsx
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const config = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+}
+
+const colorMode = extendTheme({ config })
+
+export default colorMode
---
+++ src/components/theme/index.jsx
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    customYellow: "#FFCF3C",
+  },
+  fonts: {
+    heading: "Poppins, -apple-system",
+    body: "Poppins, -apple-system",
+  },
+})
+
+export default theme
