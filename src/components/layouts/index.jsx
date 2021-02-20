ef2198b20147f3eab56550430637f6ba460f2f04
imad <imadatyatalah@gmail.com>
2021-02-20 21:18:18 +0100

    fix

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,11 +1,18 @@
 import { Box } from "@chakra-ui/react"
 
+import { MAX_WIDTH } from "../../../config"
 import Header from "./header"
 
 const Layout = ({ children }) => (
   <>
     <Header />
-    <Box as="main" minH="100vh">
+    <Box
+      as="main"
+      px={[4, 6, 10, 14, 20]}
+      maxW={MAX_WIDTH}
+      mx="auto"
+      minH="100vh"
+    >
       {children}
     </Box>
   </>
