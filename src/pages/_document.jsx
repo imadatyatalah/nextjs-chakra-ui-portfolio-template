e2d087761a38731b5cad86bfbcd0a26d41037036
imad <imadatyatalah@gmail.com>
2021-02-20 17:07:14 +0100

    add favicon

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -12,7 +12,31 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link
+            rel="shortcut icon"
+            href="/favicon/favicon.ico"
+            type="image/x-icon"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+        </Head>
         <body>
           <ColorModeScript
             initialColorMode={colorMode.config.initialColorMode}
