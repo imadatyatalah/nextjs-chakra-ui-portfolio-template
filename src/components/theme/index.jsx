c6d4d7da79763099020b43f989dfb55fc1af1315
imad <imadatyatalah@gmail.com>
2021-02-23 20:09:57 +0100

    remove customYellow

--- src/components/theme/index.jsx
+++ src/components/theme/index.jsx
@@ -1,9 +1,6 @@
 import { extendTheme } from "@chakra-ui/react"
 
 const theme = extendTheme({
-  colors: {
-    customYellow: "#FFCF3C",
-  },
   fonts: {
     heading: "Poppins, -apple-system",
     body: "Poppins, -apple-system",
