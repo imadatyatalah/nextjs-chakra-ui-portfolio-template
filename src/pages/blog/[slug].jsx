3e008567043bef77d1b7c53b22f6b56a105f0012
imad <imadatyatalah@gmail.com>
2021-02-24 20:17:58 +0100

    fetch post data from "data/blog"

---
+++ src/pages/blog/[slug].jsx
@@ -0,0 +1,31 @@
+import hydrate from "next-mdx-remote/hydrate"
+
+import { getFiles, getFileBySlug } from "../../lib/posts"
+import MDXComponents from "../../components/MDXComponents"
+
+export default function Blog({ mdxSource, frontMatter }) {
+  const content = hydrate(mdxSource, {
+    components: MDXComponents,
+  })
+
+  return <></>
+}
+
+export async function getStaticPaths() {
+  const posts = await getFiles("blog")
+
+  return {
+    paths: posts.map((p) => ({
+      params: {
+        slug: p.replace(/\.mdx/, ""),
+      },
+    })),
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const post = await getFileBySlug("blog", params.slug)
+
+  return { props: post }
+}
