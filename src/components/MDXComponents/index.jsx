53068e369d302f91dc9b94ee21794b4740a12ac5
imad <imadatyatalah@gmail.com>
2021-02-25 13:39:55 +0100

    add Headings comp

--- src/components/MDXComponents/index.jsx
+++ src/components/MDXComponents/index.jsx
@@ -2,8 +2,16 @@ import Image from "next/image"
 
 import CodeBlock from "./codeBlock"
 import CustomLink from "./customLink"
+import Headings from "./headings"
 
 const MDXComponents = {
+  h1: (props) => <Headings as="h1" size="2xl" {...props} />,
+  h2: (props) => <Headings as="h2" size="xl" {...props} />,
+  h3: (props) => <Headings as="h3" size="lg" {...props} />,
+  h4: (props) => <Headings as="h4" size="md" {...props} />,
+  h5: (props) => <Headings as="h5" size="sm" {...props} />,
+  h6: (props) => <Headings as="h6" size="xs" {...props} />,
+
   a: CustomLink,
   code: CodeBlock,
 
