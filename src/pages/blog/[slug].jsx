9e879a3a875b1a3cbbfabef7ed96c9c82d9562f3
imad <imadatyatalah@gmail.com>
2021-02-26 12:56:46 +0100

    add seo

--- src/pages/blog/[slug].jsx
+++ src/pages/blog/[slug].jsx
@@ -1,16 +1,37 @@
+import { NextSeo } from "next-seo"
 import hydrate from "next-mdx-remote/hydrate"
 
 import { getFiles, getFileBySlug } from "../../lib/posts"
+import { seo } from "../../../config"
 import MDXComponents from "../../components/MDXComponents"
+import Article from "../../components/article"
 
 export default function Blog({ mdxSource, frontMatter }) {
   const content = hydrate(mdxSource, {
     components: MDXComponents,
   })
 
+  const title = frontMatter.title
+  const siteTitle = seo.title
+
   return (
     <>
-      <article>{content}</article>
+      <NextSeo
+        title={`${title} | ${siteTitle}`}
+        description={frontMatter.summary}
+        canonical={`${seo.canonical}blog/${frontMatter.slug}`}
+        openGraph={{
+          title: title,
+          description: frontMatter.summary,
+          type: "article",
+          article: {
+            publishedTime: frontMatter.date,
+            tags: frontMatter.tags?.map((tag) => tag),
+          },
+        }}
+      />
+
+      <Article frontMatter={frontMatter}>{content}</Article>
     </>
   )
 }
@@ -19,11 +40,12 @@ export async function getStaticPaths() {
   const posts = await getFiles("blog")
 
   return {
-    paths: posts.map((p) => ({
+    paths: posts.map((post) => ({
       params: {
-        slug: p.replace(/\.mdx/, ""),
+        slug: post.replace(/\.mdx/, ""),
       },
     })),
+
     fallback: false,
   }
 }
