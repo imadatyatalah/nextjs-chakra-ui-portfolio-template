bea8f20b44f66e21ba25789cba915e4613bc2c16
imad <imadatyatalah@gmail.com>
2021-02-26 17:48:28 +0100

    replace CustomLink

--- src/components/MDXComponents/common.jsx
+++ src/components/MDXComponents/common.jsx
@@ -1,4 +1,20 @@
-import { chakra } from "@chakra-ui/react"
+import { Link as ChakraLink, chakra, Code } from "@chakra-ui/react"
+import NextLink from "next/link"
+
+export const CustomLink = (props) => {
+  const href = props.href
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"))
+
+  if (isInternalLink) {
+    return (
+      <NextLink href={href} passHref>
+        <ChakraLink {...props} />
+      </NextLink>
+    )
+  }
+
+  return <ChakraLink isExternal {...props} />
+}
 
 export const UnorderedList = (props) => (
   <>
@@ -21,5 +37,11 @@ export const OrderedList = (props) => (
 export const ListItem = (props) => (
   <>
     <chakra.li style={{ paddingBottom: "0.25rem" }} {...props} />
+  </>
+)
+
+export const InlineCode = (props) => (
+  <>
+    <Code {...props} />
   </>
 )
