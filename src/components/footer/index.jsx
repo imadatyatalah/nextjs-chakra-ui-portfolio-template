a5a68fa7ff025a1a9f4f48be56670372f088cd85
imad <imadatyatalah@gmail.com>
2021-03-08 21:19:34 +0100

    Added social media accounts

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,13 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Button, Icon } from "@chakra-ui/react"
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa"
 
 import { MAX_WIDTH } from "../../../config"
+
+const socialAccounts = [
+  { icon: FaGithub, path: "https://github.com/" },
+  { icon: FaLinkedin, path: "https://www.linkedin.com/" },
+  { icon: FaInstagram, path: "https://www.instagram.com/" },
+]
 
 const Footer = () => (
   <>
@@ -12,7 +19,15 @@ const Footer = () => (
         px={[4, 6, 10, 14, 20]}
         maxW={MAX_WIDTH}
         mx="auto"
-      ></Box>
+      >
+        {socialAccounts.map((item, index) => (
+          <Button mx="2" key={index}>
+            <a href={item.path} target="_blank" rel="noopener noreferrer">
+              <Icon as={item.icon} w="6" h="6" />
+            </a>
+          </Button>
+        ))}
+      </Box>
     </Box>
   </>
 )
