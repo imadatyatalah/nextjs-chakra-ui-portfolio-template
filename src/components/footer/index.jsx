e23086634f278b1f1f2e95182614931f1b9b41ae
imad <imadatyatalah@gmail.com>
2021-03-09 12:03:45 +0100

    Fix

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,9 @@
-import { Box, Button, Icon } from "@chakra-ui/react"
-import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa"
+import { Box, Button, Icon, Link as ChakraLink, Text } from "@chakra-ui/react"
+import { FaGithub, FaLinkedin, FaInstagram, FaCode } from "react-icons/fa"
 
 import { MAX_WIDTH } from "../../../config"
 
+// Fell free to add your social media accounts!
 const socialAccounts = [
   { icon: FaGithub, path: "https://github.com/" },
   { icon: FaLinkedin, path: "https://www.linkedin.com/" },
@@ -16,17 +17,56 @@ const Footer = () => (
         display="flex"
         justifyContent="center"
         alignItems="center"
+        flexDir="column"
+        textAlign="center"
+        minH="4rem"
         px={[4, 6, 10, 14, 20]}
         maxW={MAX_WIDTH}
         mx="auto"
       >
-        {socialAccounts.map((item, index) => (
-          <Button mx="2" key={index}>
-            <a href={item.path} target="_blank" rel="noopener noreferrer">
-              <Icon as={item.icon} w="6" h="6" />
-            </a>
-          </Button>
-        ))}
+        <Box py="2">
+          {socialAccounts.map((item, index) => (
+            <ChakraLink href={item.path} key={index} isExternal>
+              <Button mx="2">
+                <Icon as={item.icon} w="6" h="6" />
+              </Button>
+            </ChakraLink>
+          ))}
+        </Box>
+        <Box py="2">
+          <Text>
+            Built with{" "}
+            <span role="img" aria-label="red heart">
+              ❤️
+            </span>
+            ,{" "}
+            <ChakraLink href="https://nextjs.org/" fontWeight="600" isExternal>
+              Next.js
+            </ChakraLink>
+            ,{" "}
+            <ChakraLink
+              href="https://chakra-ui.com/"
+              fontWeight="600"
+              isExternal
+            >
+              Chakra UI
+            </ChakraLink>
+            , Hosted in{" "}
+            <ChakraLink href="http://vercel.com/" fontWeight="600" isExternal>
+              Vercel
+            </ChakraLink>
+          </Text>
+        </Box>
+        <Box py="2">
+          <ChakraLink
+            href="https://github.com/imadatyatalah/portfolio-boilerplate-nextjs"
+            isExternal
+          >
+            <Button>
+              <Icon as={FaCode} w="6" h="6" />
+            </Button>
+          </ChakraLink>
+        </Box>
       </Box>
     </Box>
   </>
