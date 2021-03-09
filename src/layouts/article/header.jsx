7c38fc4811433010320d792e8d416a54de38ef0b
imad <imadatyatalah@gmail.com>
2021-03-09 12:03:32 +0100

    Replace article body

---
+++ src/layouts/article/header.jsx
@@ -0,0 +1,62 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Tag,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react"
+import NextLink from "next/link"
+import dayjs from "dayjs"
+
+import { tagColor } from "../../components/UI/tagColor"
+
+const Header = ({ frontMatter }) => {
+  const color = useColorModeValue("gray.700", "gray.400")
+
+  return (
+    <>
+      <Box as="header" textAlign="center">
+        <Heading as="h1" py="4" size="2xl">
+          {frontMatter.title}
+        </Heading>
+
+        <Flex direction="column">
+          <Text color={color} py="1">
+            {frontMatter.author} /{" "}
+            {dayjs(frontMatter.date).format("MMMM DD, YYYY")} /{" "}
+            {frontMatter.readingTime.text}
+          </Text>
+          <Text py="1">
+            {frontMatter.tags.map((tag) => {
+              const color = tagColor[tag]
+
+              return (
+                <NextLink href={`/blog/tags/${tag}`} key={tag}>
+                  <a>
+                    <Tag
+                      m="1"
+                      size="lg"
+                      pb="px"
+                      bgColor={color.bgColor}
+                      color={color.color}
+                      transitionDuration="250ms"
+                      _hover={{
+                        cursor: "pointer",
+                        backgroundColor: color.hover,
+                      }}
+                    >
+                      {tag}
+                    </Tag>
+                  </a>
+                </NextLink>
+              )
+            })}
+          </Text>
+        </Flex>
+      </Box>
+    </>
+  )
+}
+
+export default Header
