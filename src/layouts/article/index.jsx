1b501ab10d77511af35bf9cbf947488ba5c38979
imad <imadatyatalah@gmail.com>
2021-03-10 11:24:14 +0100

    Added header to the index.jsx file

--- src/layouts/article/index.jsx
+++ src/layouts/article/index.jsx
@@ -1,23 +1,75 @@
-import { Box } from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import {
+  Box,
+  Flex,
+  Heading,
+  Tag,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react"
 import { MDXProvider } from "@mdx-js/react"
+import dayjs from "dayjs"
 
+import { tagColor } from "../../components/UI/tagColor"
 import MDXComponents from "../../components/MDXComponents"
-import Header from "./header"
 
-const Article = ({ children, ...props }) => (
-  <>
-    <MDXProvider components={MDXComponents}>
-      <Box
-        as="section"
-        px={{ md: "10", lg: "20", xl: "40" }}
-        py="4"
-        fontSize="16px"
-      >
-        <Header {...props} />
-        <article>{children}</article>
-      </Box>
-    </MDXProvider>
-  </>
-)
+const Article = ({ children, frontMatter }) => {
+  const router = useRouter()
+  const color = useColorModeValue("gray.700", "gray.400")
+
+  return (
+    <>
+      <MDXProvider components={MDXComponents}>
+        <Box as="section" px={{ md: "10", lg: "20", xl: "40" }} py="4">
+          <Box as="header" textAlign="center">
+            <Heading as="h1" py="4" size="2xl">
+              {frontMatter.title}
+            </Heading>
+
+            <Flex direction="column">
+              <Text fontSize="16px" color={color} py="1">
+                {frontMatter.author} /{" "}
+                {dayjs(frontMatter.date).format("MMMM DD, YYYY")} /{" "}
+                {frontMatter.readingTime.text}
+              </Text>
+              <Text py="1">
+                {frontMatter.tags.map((tag) => {
+                  const color = tagColor[tag]
+
+                  return (
+                    <Tag
+                      m="1"
+                      size="lg"
+                      pt="px"
+                      pb="1"
+                      bgColor={color.bgColor}
+                      color={color.color}
+                      transitionDuration="250ms"
+                      cursor="pointer"
+                      _hover={{
+                        backgroundColor: color.hover,
+                      }}
+                      onClick={() =>
+                        router.push({
+                          pathname: "/blog/",
+                          query: { tag },
+                        })
+                      }
+                      key={tag}
+                    >
+                      {tag}
+                    </Tag>
+                  )
+                })}
+              </Text>
+            </Flex>
+          </Box>
+
+          <Box as="article">{children}</Box>
+        </Box>
+      </MDXProvider>
+    </>
+  )
+}
 
 export default Article
