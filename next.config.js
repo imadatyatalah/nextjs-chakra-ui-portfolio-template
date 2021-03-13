4537b0465189fade9f43bd02acd14ad950d5b3b1
imad <imadatyatalah@gmail.com>
2021-03-13 09:57:24 +0100

    Override next.config.js to run the generate-sitemap script as part of the build process.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 })
 
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./src/scripts/generate-sitemap.jsx")
+    }
+
+    return config
+  },
+}
+
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
 })
