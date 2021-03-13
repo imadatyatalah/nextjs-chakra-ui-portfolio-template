680306797d6b5f783c6b4952277b5826b3da2a5f
imad <imadatyatalah@gmail.com>
2021-03-13 12:42:22 +0100

    Remove min height

--- src/layouts/global.jsx
+++ src/layouts/global.jsx
@@ -7,13 +7,7 @@ import Footer from "../components/footer"
 const Layout = ({ children }) => (
   <>
     <Header />
-    <Box
-      as="main"
-      px={[4, 6, 10, 14, 20]}
-      maxW={MAX_WIDTH}
-      mx="auto"
-      minH="100vh"
-    >
+    <Box as="main" px={[4, 6, 10, 14, 20]} maxW={MAX_WIDTH} mx="auto">
       {children}
     </Box>
     <Footer />
