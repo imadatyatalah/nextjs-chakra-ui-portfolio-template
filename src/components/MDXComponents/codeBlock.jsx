5ecbebde167428d2122678ed543374a07c2e11b2
imad <imadatyatalah@gmail.com>
2021-03-21 13:04:08 +0100

    Added copy button

--- src/components/MDXComponents/codeBlock.jsx
+++ src/components/MDXComponents/codeBlock.jsx
@@ -1,4 +1,4 @@
-import { useColorMode } from "@chakra-ui/react"
+import { Box, Button, useClipboard, useColorMode } from "@chakra-ui/react"
 import Highlight, { defaultProps } from "prism-react-renderer"
 import lightTheme from "prism-react-renderer/themes/nightOwlLight"
 import darkTheme from "prism-react-renderer/themes/nightOwl"
@@ -7,36 +7,51 @@ import styles from "./styles/codeBlock.module.css"
 
 const CodeBlock = ({ children, className }) => {
   const { colorMode } = useColorMode()
+  const { hasCopied, onCopy } = useClipboard(children)
 
   const language = className.replace(/language-/, "")
 
   return (
     <>
-      <Highlight
-        {...defaultProps}
-        theme={colorMode === "dark" ? darkTheme : lightTheme}
-        code={children}
-        language={language}
-      >
-        {({ className, style, tokens, getLineProps, getTokenProps }) => (
-          <pre
-            className={`${className} ${styles.wrapper}`}
-            style={{ ...style }}
-          >
-            {tokens.map((line, i) => (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                <span className={styles.lineNo}>{i + 1}</span>
+      <Box pos="relative">
+        <Button
+          fontFamily="Poppins"
+          colorScheme="twitter"
+          pos="absolute"
+          right="2"
+          top="2"
+          size="sm"
+          onClick={onCopy}
+        >
+          {hasCopied ? "Copied" : "Copy"}
+        </Button>
 
-                <span className={styles.lineContent}>
-                  {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token, key })} />
-                  ))}
-                </span>
-              </div>
-            ))}
-          </pre>
-        )}
-      </Highlight>
+        <Highlight
+          {...defaultProps}
+          theme={colorMode === "dark" ? darkTheme : lightTheme}
+          code={children}
+          language={language}
+        >
+          {({ className, style, tokens, getLineProps, getTokenProps }) => (
+            <pre
+              className={`${className} ${styles.wrapper}`}
+              style={{ ...style }}
+            >
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  <span className={styles.lineNo}>{i + 1}</span>
+
+                  <span className={styles.lineContent}>
+                    {line.map((token, key) => (
+                      <span key={key} {...getTokenProps({ token, key })} />
+                    ))}
+                  </span>
+                </div>
+              ))}
+            </pre>
+          )}
+        </Highlight>
+      </Box>
     </>
   )
 }
