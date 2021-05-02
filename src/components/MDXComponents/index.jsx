95f0aabf1baccc345c5a18678c2ce16fbe71da4d
imad <imadatyatalah@gmail.com>
2021-05-02 13:43:36 +0000

    Fix Headings sizes

--- src/components/MDXComponents/index.jsx
+++ src/components/MDXComponents/index.jsx
@@ -11,12 +11,12 @@ import Headings from "./headings"
 import CodeBlock from "./codeBlock"
 
 const MDXComponents = {
-  h1: (props) => <Headings as="h1" fontSize="40px" {...props} />,
-  h2: (props) => <Headings as="h2" fontSize="30px" {...props} />,
-  h3: (props) => <Headings as="h3" fontSize="26px" {...props} />,
-  h4: (props) => <Headings as="h4" fontSize="22px" {...props} />,
-  h5: (props) => <Headings as="h5" fontSize="18px" {...props} />,
-  h6: (props) => <Headings as="h6" fontSize="16px" {...props} />,
+  h1: (props) => <Headings as="h1" fontSize="36px" {...props} />,
+  h2: (props) => <Headings as="h2" fontSize="28px" {...props} />,
+  h3: (props) => <Headings as="h3" fontSize="24px" {...props} />,
+  h4: (props) => <Headings as="h4" fontSize="18px" {...props} />,
+  h5: (props) => <Headings as="h5" fontSize="16px" {...props} />,
+  h6: (props) => <Headings as="h6" fontSize="14px" {...props} />,
 
   code: CodeBlock,
   a: CustomLink,
