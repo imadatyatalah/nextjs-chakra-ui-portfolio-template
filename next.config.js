815c46f88895fa2f407bb5886759d928628a9f9f
imad <imadatyatalah@gmail.com>
2021-06-23 11:22:15 +0100

    fix #18 Coses #18

--- next.config.js
+++ next.config.js
@@ -2,16 +2,14 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 })
 
-module.exports = {
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "mdx"],
+
   webpack: (config, { isServer }) => {
     if (isServer) {
-      require("./src/scripts/generate-sitemap.jsx")
+      require("./src/scripts/generate-sitemap")
     }
 
     return config
   },
-}
-
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "mdx"],
 })
