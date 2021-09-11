20ad7aa9ef1cf62db1944360a7349b316bf19a4c
imad <imadatyatalah@gmail.com>
2021-09-11 21:39:54 +0100

    fix: Use import instead of require

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -1,27 +1,28 @@
-import fs from "fs"
+import { readFileSync, readdirSync } from "fs"
 import matter from "gray-matter"
 import path from "path"
 import readingTime from "reading-time"
 import renderToString from "next-mdx-remote/render-to-string"
+import remarkSlug from "remark-slug"
 
 import MDXComponents from "@/components/MDXComponents"
 
 const root = process.cwd()
 
 export async function getFiles(type) {
-  return fs.readdirSync(path.join(root, "src/data", type))
+  return readdirSync(path.join(root, "src/data", type))
 }
 
 export async function getFileBySlug(type, slug) {
   const source = slug
-    ? fs.readFileSync(path.join(root, "src/data", type, `${slug}.mdx`), "utf8")
-    : fs.readFileSync(path.join(root, "src/data", `${type}.mdx`), "utf8")
+    ? readFileSync(path.join(root, "src/data", type, `${slug}.mdx`), "utf8")
+    : readFileSync(path.join(root, "src/data", `${type}.mdx`), "utf8")
 
   const { data, content } = matter(source)
   const mdxSource = await renderToString(content, {
     components: MDXComponents,
     mdxOptions: {
-      remarkPlugins: [require("remark-slug"), require("remark-code-titles")],
+      remarkPlugins: [remarkSlug, require("remark-code-titles")],
     },
   })
 
@@ -37,10 +38,10 @@ export async function getFileBySlug(type, slug) {
 }
 
 export async function getAllFilesFrontMatter(type) {
-  const files = fs.readdirSync(path.join(root, "src/data", type))
+  const files = readdirSync(path.join(root, "src/data", type))
 
   return files.reduce((allPosts, postSlug) => {
-    const source = fs.readFileSync(
+    const source = readFileSync(
       path.join(root, "src/data", type, postSlug),
       "utf8"
     )
